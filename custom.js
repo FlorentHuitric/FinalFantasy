fde2273f8e0038b989d072775f45e70d4068e59a
FuroRen <88617899+FuroRen@users.noreply.github.com>
2022-11-08 17:04:09 +0100

    first commit

---
+++ custom.js
@@ -0,0 +1,5 @@
+
+
+
+// Problème résolu avec bootstrap
+alert("Ce site est a pour but de vous renseigner sur l'une des séries les plus emblématiques du jeu vidéo. J'espère que vous aurez autant de plaisir à le parcourir que moi à le créer. Ceci est un projet expérimental qui a pour but de mobiliser mes compétences avec le framework Bootstrap et de servir de présentation pour mes futures recherches d'emploi.");
